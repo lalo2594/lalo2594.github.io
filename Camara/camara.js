69be39cbbc4cd38452b7cca7871ee71e58ab8d1f
lalo2594 <lalo.259472@gmail.com>
2017-03-09 13:20:44 -0600

    Camaras perspectiva y ortografica
    se realizaron los ejemplos usando estos dos tipos de camara.

--- Camara/camara.js
+++ Camara/camara.js
@@ -5,10 +5,20 @@ var planoLejano=1000;
 var camara=new THREE.PerspectiveCamera(campoVision,relacionAspecto,planoCercano,planoLejano);
 camara.position.z=15;
 
-var cubo=new THREE.Mesh(new THREE.BoxGeometry(2,2,2));
+var cubo=new THREE.Mesh(new THREE.BoxGeometry(2,2,2),new THREE.MeshNormalMaterial());
 cubo.rotateY(Math.PI/4);
 var esfera1=new THREE.Mesh(new THREE.SphereGeometry(1),new THREE.MeshNormalMaterial());
 esfera1.position.x=5;
 var esfera2=new THREE.Mesh(new THREE.SphereGeometry(1),new THREE.MeshNormalMaterial());
-esfera1.position.x=-5;
-esfera1.position.z=-10;
+esfera2.position.x=-5;
+esfera2.position.z=-10;
+
+var escena=new THREE.Scene();
+escena.add(esfera1);
+escena.add(esfera2);
+escena.add(cubo);
+
+var renderizador=new THREE.WebGLRenderer();
+renderizador.setSize(window.innerWidth,window.innerHeight);
+document.body.appendChild(renderizador.domElement);
+renderizador.render(escena,camara);
